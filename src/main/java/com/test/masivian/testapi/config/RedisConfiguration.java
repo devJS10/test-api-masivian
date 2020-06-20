bd8acb487abd62fa92f328aa54afa5aac9f07741
lucho10 <luisjavierosorio3@gmail.com>
2020-06-19 21:08:10 -0500

    redis configurado

---
+++ src/main/java/com/test/masivian/testapi/config/RedisConfiguration.java
@@ -0,0 +1,25 @@
+package com.test.masivian.testapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.test.masivian.testapi.model.Roulette;
+
+@Configuration
+public class RedisConfiguration {
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	RedisTemplate<String, Roulette> redisTemplate() {
+		final RedisTemplate<String, Roulette> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
+
+}
---
+++ src/main/java/com/test/masivian/testapi/model/Roulette.java
@@ -0,0 +1,31 @@
+package com.test.masivian.testapi.model;
+
+public class Roulette {
+
+	private String id;
+	private String color;
+	private boolean status;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+}
